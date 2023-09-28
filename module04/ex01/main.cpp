82e2dccb2ac91f75ef43ea279286fa622c045aaf
IOK <sydauria@student.42.fr>
2023-09-28 15:06:04 +0200

    updating test

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: greengo <greengo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 16:31:35 by greengo           #+#    #+#             */
-/*   Updated: 2023/09/28 12:30:27 by greengo          ###   ########.fr       */
+/*   Updated: 2023/09/28 15:00:46 by greengo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 #include "WrongCat.hpp"
 
 int main( void ) {
-    
+
+
 //const Animal* meta = new Animal();
 //Animal* Rex = new Dog();
-Animal* Canelle = new Cat();
+Animal *Canelle = new Cat();
 //Animal* Nuve = new Cat();
-Animal* Nuve = Canelle;
 
 //std::cout << Rex->gettype() << " " << std::endl;
 //Rex->makeSound();
@@ -29,11 +29,19 @@ Animal* Nuve = Canelle;
 //Canelle->makeSound(); //will output the cat sound!
 //std::cout << meta->gettype() << std::endl;
 //meta->makeSound();
-Canelle->setIdea("Beautifull day");
+Canelle->setIdea("*Beautifull day*");
 std::cout << "Canelle is thinking: " << Canelle->getIdea(0) << std::endl;
+std::cout << "\033[31m=== Polymorphing cloning starting [new Animal Nuve will be create by cloning cat. Nuve should be think like Canelle] ===\033[0m" << std::endl;  
+Animal *Nuve = Canelle->clone();
 std::cout << "Nuve is thinking: " << Nuve->getIdea(0) << std::endl;
-Canelle->setIdea("OUAIS OUAIS OUAIS");
+std::cout << "\033[31m=== Set new idea [Nuve look like a good fella] to Canelle. Nuve should not think here ===\033[0m" << std::endl;
+Canelle->setIdea("*Nuve look like a good fella*");
 std::cout << "Nuve is thinking: " << Nuve->getIdea(1) << std::endl;
+std::cout << "Canelle is thinking: " << Canelle->getIdea(1) << std::endl;
+std::cout << "\033[31m===Set the new idea [Qu'est ce que c'est trop beau la vie d'artiste] to Nuve. Canelle should not change ideas\033[0m" << std::endl;
+Nuve->setIdea("*Qu'est ce que c'est trop beau la vie d'artiste*");
+std::cout << "Nuve is thinking: " << Nuve->getIdea(1) << std::endl;
+std::cout << "Canelle is thinking: " << Canelle->getIdea(1) << std::endl;
 //Nuve->setIdea("Not good day.");
 
 
