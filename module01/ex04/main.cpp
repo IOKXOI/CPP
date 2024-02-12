8f33b96595b793d8055de633fe109eeb5fa426d2
greengo <sydauria@student.42.fr>
2024-02-12 23:54:16 +0100

    finish ex04

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -14,24 +14,39 @@
 #include <iostream>
 #include <fstream>
 
+void replaceOccurrences(std::string& str, const std::string& s1, const std::string& s2) {
+    size_t pos = 0;
+    while ((pos = str.find(s1, pos)) != std::string::npos) {
+        str.erase(pos, s1.length());
+        str.insert(pos, s2);
+        pos += s2.length();
+    }
+}
+
 int main(int argc, char** argv) {
     if (argc != 4) {
         std::cout << "Invalid parameters" << std::endl;
         return (1);
     }
-    std::ifstream file(argv[1]);
-    if (file.fail()) {
+    std::ifstream oldFile(argv[1]);
+    if (oldFile.fail()) {
         std::cout << ("Open error: Cannot open ") << std::string(argv[1]) << std::endl;
-        exit(1);
+        return(1);
     }
     std::string     destinationName((std::string)argv[1] + ".replace");
-    std::ofstream   destination(destinationName);
-    if (destination.fail()) {
+    std::ofstream   newFile(destinationName.c_str());
+    if (newFile.fail()) {
         std::cout << ("Open error: Cannot open ") << destinationName << std::endl;
-        exit(1);
+        return(1);
     }
-    std::string expression2(argv[2]);
-    std::string expression1(argv[3]);
-    while ()
+    std::string s1(argv[2]);
+    std::string s2(argv[3]);
+    std::string line;
+    while (std::getline(oldFile, line)) {
+        replaceOccurrences(line, s1, s2);
+        newFile << line << std::endl;
+    }
+	oldFile.close();
+	newFile.close();
 
 }
