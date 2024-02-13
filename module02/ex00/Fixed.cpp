0c89444bfe29e8d317ecfa06c45b25d9fa369a53
IOK <sydauria@student.42.fr>
2024-02-13 20:48:37 +0100

    add ex00 module2

---
+++ module02/ex00/Fixed.cpp
@@ -0,0 +1,33 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed(): _integer(0) {
+	std::cout << "Fixed default constructor called." << std::endl;
+}
+
+Fixed::Fixed(const Fixed &toCopy) {
+	std::cout << "Fixed copy constructor called." << std::endl;
+	_integer = toCopy.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed&toCopy) {
+	std::cout << "Fixed assignment constructor called." << std::endl;
+	if (this != &toCopy) {
+		_integer = toCopy.getRawBits();
+	}
+	return (*this);
+}
+
+Fixed::~Fixed() {
+	std::cout << "Default destructor called." << std::endl;
+}
+
+int Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_integer);
+}
+
+void Fixed::setRawBits( int const raw ) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_integer = raw;
+}
