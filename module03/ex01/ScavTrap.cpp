2c3b1e20c47144ee4fc5d1785cab239d60ae6fe1
greengo <sydauria@student.42.fr>
2024-02-16 18:04:41 +0100

    ScavTrap

--- module03/ex01/ScavTrap.cpp
+++ module03/ex01/ScavTrap.cpp
@@ -1,10 +1,10 @@
 #include "ScavTrap.hpp"
 
-ScavTrap::ScavTrap() {
+ScavTrap::ScavTrap(): ClapTrap() {
     std::cout << "ScavTrap default constructor called." << std::endl;
 }
 
-ScavTrap::ScavTrap(const ScavTrap &toCopy) {
+ScavTrap::ScavTrap(const ScavTrap &toCopy): ClapTrap() {
     std::cout << "ScavTrap copie constructor called." << std::endl;
     _name = toCopy.getName();
     _attack = toCopy.getAttack();
@@ -29,4 +29,25 @@ ScavTrap::~ScavTrap() {
 
 ScavTrap::ScavTrap(std::string name): _name(name), _hp(100), _energy(50), _attack(20) {
     std::cout << "ScavTrap named constructor called." << std::endl;
+}
+
+
+std::string ScavTrap::getName() const{
+    return (_name);
+}
+
+uint32_t     ScavTrap::getAttack() const{
+    return(_attack);
+}
+
+uint32_t     ScavTrap::getEnergy() const{
+    return(_energy);
+}
+
+int32_t     ScavTrap::getHp() const{
+    return(_hp);
+}
+
+void    ScavTrap::guardGate(){
+    std::cout << "I'am the guardian !" << std::endl;
 }
